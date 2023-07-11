8d80c19da71ee2d6ed599d60c07f8c795a0e1a65
francescosongia <francescosongia@mail.polimi.it>
2023-07-11 15:04:23 +0200

    before starting LA

--- include/ras.hpp
+++ include/ras.hpp
@@ -171,4 +171,83 @@ public:
 };
 
 
+////////
+/*
+class ParallelLA : public Ras<ParallelLA>
+{
+  public:
+    Parallel(Domain dom, const Decomposition& dec,const LocalMatrices local_matrices,const SolverTraits& traits) :
+     Ras<ParallelLA>(dom,dec,local_matrices,traits) {};
+
+    Eigen::VectorXd 
+    precondAction(const SpMat& x) 
+    {
+      Eigen::VectorXd z=Eigen::VectorXd::Zero(domain.nln()*domain.nt()*domain.nx()*2);
+      Eigen::VectorXd uk(domain.nln()*DataDD.sub_sizes()[0]*DataDD.sub_sizes()[1]*2);
+
+      // questo for devo prendere i k giusti che vede quel core, mi appoggio su sub_assingment
+      auto sub_division_vec = local_mat.sub_assignment().sub_division_vec()[local_mat.rank()];
+      for(unsigned int k : sub_division_vec){    
+          Eigen::SparseLU<SpMat > lu;
+          lu.compute(local_mat.getAk(k));
+          auto temp = local_mat.getRk(k);
+          uk = lu.solve(temp.first*x);
+          z=z+(temp.second.transpose())*uk;
+          }
+
+      MPI_Allreduce(MPI_IN_PLACE, z.data(), domain.nln()*domain.nt()*domain.nx()*2, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+      return z;
+    }
+
+
+    SolverResults solve(const SpMat& A, const SpMat& b)
+    {
+      int rank{0},size{0};
+      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+      MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+      auto start = std::chrono::steady_clock::now();
+      double tol=this->traits_.tol();
+      unsigned int max_it=this->traits_.max_it();
+      double res=tol+1;
+      std::vector<double> relres2P_vec;
+      unsigned int niter=0;
+      double Pb2=precondAction(b).norm();
+
+      Eigen::VectorXd uw0=Eigen::VectorXd::Zero(this->domain.nln()*this->domain.nt()*this->domain.nx()*2);
+      Eigen::VectorXd uw1(this->domain.nln()*this->domain.nt()*this->domain.nx()*2);
+
+      //scatter A, ottengo a_intraparallel_1_2
+
+
+
+      Eigen::VectorXd z= precondAction(b); //b-A*uw0
+      while(res>tol and niter<max_it){
+          uw1=uw0+z;  //forse si puo evitare l'uso di entrambi uw0 e uw1
+          z= precondAction(b-A*uw1);
+          res=(z/Pb2).norm();
+          relres2P_vec.push_back(res);
+          niter++;
+          uw0=uw1;
+      }
+
+      auto end = std::chrono::steady_clock::now();
+      
+      unsigned int solves{0};
+      double time{0.0};
+      if(rank==0){
+      std::cout<<"niter: "<<niter<<std::endl;
+      solves = niter*this->DataDD.nsub();
+      std::cout<<"solves: "<<solves<<std::endl;
+      time = std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
+      std::cout <<"time in seconds: "<< time<<std::endl;
+      }
+      SolverResults res_obj(uw1,solves,time, this->traits_, DataDD);
+
+      return res_obj;
+    };
+};
+*/
+
+
 #endif
