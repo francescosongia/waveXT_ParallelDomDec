a3aa372162fe53e79799f91d9f2601dc5f5eb158
NNrico <100316667+NNrico@users.noreply.github.com>
2023-08-04 18:26:13 +0200

    Update local_matrices.hpp

--- include/local_matrices.hpp
+++ include/local_matrices.hpp
@@ -63,28 +63,32 @@ public:
     auto size_assigned = sub_division_vec.size();
 
     // parallel framework
-    if(size_assigned < this->DataDD.nsub() && this->sub_assignment_.custom_matrix()==false){     
+    if(size_assigned < this->DataDD.nsub() && this->sub_assignment_.custom_matrix()==false)
+    {     
         
         this->R_.resize(size_assigned);
         this->R_tilde_.resize(size_assigned);
         this->localA_.resize(size_assigned);
         unsigned int k_local{0};
-        for(unsigned int k : sub_division_vec){
+        for(unsigned int k : sub_division_vec)
+        {
 
             std::pair<SpMat, SpMat> res= this->createRK(k);
             k_local++;
             this->R_[k_local-1] = std::move(res.first);
             this->R_tilde_[k_local-1] = std::move(res.second);
             
-            }  
+        }  
     }
     //sequential framework
-    else{
-        for(unsigned int k : sub_division_vec){
+    else
+    {
+        for(unsigned int k : sub_division_vec)
+        {
             std::pair<SpMat, SpMat> res= this->createRK(k);
             this->R_[k-1]= std::move(res.first);
             this->R_tilde_[k-1] = std::move(res.second);
-            }
+        }
     }
   }; 
 
@@ -104,11 +108,13 @@ public:
         std::cerr<<"k not valid"<<std::endl;
         return {1,1};
     }
-    if (this->localA_created_==0) {
+    if (this->localA_created_==0) 
+    {
         std::cerr << "local A not created" << std::endl;
         return {1, 1};
     }
-    else{
+    else
+    {
         auto iscustom = this->sub_assignment_.custom_matrix();
         auto local_k = (!iscustom)? this->sub_assignment_.idxSub_to_LocalNumbering(k, this->current_rank_):k;
         return this->localA_[local_k-1];
@@ -149,7 +155,8 @@ private:
     Eigen::ArrayXi indexcol = Eigen::ArrayXi::Zero(nln*m*n*2);
     Eigen::ArrayXd values = Eigen::ArrayXd::Zero(nln*m*n*2);
 
-    for (size_t q=0;q<=n-1;++q) {
+    for (size_t q=0;q<=n-1;++q) 
+    {
         auto temp = Eigen::VectorXi::LinSpaced(nln * m, q * nt * nln + (start_elem - 1) * nln,
                                                (q * nt + m) * nln + (start_elem - 1) * nln-1);
         indexcol(Eigen::seq(q * nln * m, nln * m * (q + 1)-1)) << temp;
@@ -159,13 +166,16 @@ private:
     auto temp1 = Eigen::ArrayXd::Constant(ot_back*nln,(1-theta)/2);
     auto temp2 = Eigen::ArrayXd::Constant((m-ot_forw-ot_back)*nln,0.5);
     auto temp3 = Eigen::ArrayXd::Constant(ot_forw*nln,theta/2);
-    for(size_t q=0;q<ox_back;++q){
+    for(size_t q=0;q<ox_back;++q)
+    {
         values(Eigen::seq(q*nln*m,nln*m*(q+1)-1))<<temp1,temp2,temp3;
     }
-    for(size_t q=ox_back;q<n-ox_forw;++q){
+    for(size_t q=ox_back;q<n-ox_forw;++q)
+    {
         values(Eigen::seq(q*nln*m,nln*m*(q+1)-1))<<temp1*2,temp2*2,temp3*2;
     }
-    for(size_t q=n-ox_forw;q<n;++q){
+    for(size_t q=n-ox_forw;q<n;++q)
+    {
         values(Eigen::seq(q*nln*m,nln*m*(q+1)-1))<<temp1,temp2,temp3;
     }
     values(Eigen::seq(nln*m*n,nln*m*n*2 - 1))=values(Eigen::seq(0,nln*m*n-1)); //extend for w
@@ -174,7 +184,8 @@ private:
     tripletList.reserve(nln*m*n*2);
     std::vector<T> tripletList_tilde;
     tripletList_tilde.reserve(nln*m*n*2);
-    for(size_t i=0;i<nln*m*n*2;++i){
+    for(size_t i=0;i<nln*m*n*2;++i)
+    {
         tripletList.emplace_back(i,indexcol[i],1.);
         tripletList_tilde.emplace_back(i,indexcol[i],values[i]);
     }
@@ -196,10 +207,9 @@ private:
     SpMat temp(nln*m*n*2,nln*nt*nx*2);
     auto sub_division_vec = this->sub_assignment_.sub_division_vec()[this->current_rank_];
     auto iscustom = this->sub_assignment_.custom_matrix();
-    for(unsigned int k : sub_division_vec){   
-        
+    for(unsigned int k : sub_division_vec)
+    {   
         auto local_k = (!iscustom)? this->sub_assignment_.idxSub_to_LocalNumbering(k, this->current_rank_):k;
-
         temp=this->R_[local_k-1]*A;
         this->localA_[local_k-1]=temp*(this->R_[local_k-1].transpose());
     }
