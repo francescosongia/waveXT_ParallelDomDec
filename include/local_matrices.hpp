b74b2500c9409713800608520cf2e42d1d4b54ab
NNrico <100316667+NNrico@users.noreply.github.com>
2023-08-05 13:29:14 +0200

    Update local_matrices.hpp

--- include/local_matrices.hpp
+++ include/local_matrices.hpp
@@ -30,7 +30,7 @@ class LocalMatrices {
 public:
 
   LocalMatrices(Domain dom,const Decomposition&  dec,const SpMat& A, int np, int current_rank=0) :
-    domain(dom),DataDD(std::move(dec)),R_(DataDD.nsub()),R_tilde_(DataDD.nsub()), localA_(DataDD.nsub()),localA_created_(0),
+    domain(dom),DataDD(dec),R_(DataDD.nsub()),R_tilde_(DataDD.nsub()), localA_(DataDD.nsub()),localA_created_(0),
     current_rank_(current_rank), sub_assignment_(LA(np,DataDD.nsub_x(),DataDD.nsub_t()))
   {   
            
@@ -45,7 +45,7 @@ public:
 
   // constructor for custom matrix in sub assignment
   LocalMatrices(Domain dom,const Decomposition&  dec,const SpMat& A, int np, int current_rank, Eigen::MatrixXi custom_mat) :
-    domain(dom),DataDD(std::move(dec)),R_(DataDD.nsub()),R_tilde_(DataDD.nsub()), localA_(DataDD.nsub()),localA_created_(0),
+    domain(dom),DataDD(dec),R_(DataDD.nsub()),R_tilde_(DataDD.nsub()), localA_(DataDD.nsub()),localA_created_(0),
     current_rank_(current_rank),sub_assignment_(LA(np,DataDD.nsub_x(),DataDD.nsub_t(),custom_mat))
   {   
      
